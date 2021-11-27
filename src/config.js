463b6bf793d7d6cd9c137b866a4bf0f2f9df80a6
yxynicole <ye.xinyu@northeastern.edu>
2021-11-26 23:11:10 -0500

    change host to remote

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ const HOSTS = {
     local:'http://localhost:4000',
     heroku: 'https://thawing-hollows-98347.herokuapp.com'
 }
-const BACKEND_URL = HOSTS.local
+const BACKEND_URL = HOSTS.heroku
 
 export default BACKEND_URL
